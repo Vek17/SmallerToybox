50439efbd11ca975370fc99017ce8f896958678a
Sean Petrie <sean.petrie17@gmail.com>
2021-10-13 14:29:09 -0500

    Purged loot

--- ToyBox/classes/MainUI/Main.cs
+++ ToyBox/classes/MainUI/Main.cs
@@ -143,7 +143,6 @@ namespace ToyBox {
                     new NamedAction("Bag of Tricks", () => BagOfTricks.OnGUI()),
                     new NamedAction("Level Up", () => LevelUp.OnGUI()),
                     new NamedAction("Party", () => PartyEditor.OnGUI()),
-                    new NamedAction("Loot", () => PhatLoot.OnGUI()),
                     new NamedAction("Enchantment", () => EnchantmentEditor.OnGUI()),
 #if false
                     new NamedAction("Playground", () => Playground.OnGUI()),
